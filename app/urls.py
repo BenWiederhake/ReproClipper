44ac009568b09cf130746b9de27e3779612a86ef
Ben Wiederhake <BenWiederhake.GitHub@gmx.de>
2026-07-11 15:47:59 +0200

    Create stub pages, write URL patterns

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,14 @@
-from django.urls import path
+from django.urls import include, path
 
 from . import views
 
+project_patterns = [
+    path("", views.project_index, name="project_index"),
+    path("next_open", views.next_open, name="next_open"),
+    path("v/<int:version_id>/", views.specific_version, name="specific_version"),
+]
+
 urlpatterns = [
     path("", views.index, name="index"),
+    path("p/<slug:project_slug>/", include(project_patterns)),
 ]
--- app/views.py
+++ app/views.py
@@ -4,3 +4,15 @@ from django.shortcuts import render
 
 def index(request):
     return HttpResponse("Hello, world. You're at the repro_clipper index.")
+
+
+def project_index(request, project_slug: str):
+    return HttpResponse(f"This is about project {project_slug}.")
+
+
+def next_open(request, project_slug: str):
+    return HttpResponse(f"Next open of project {project_slug}.")
+
+
+def specific_version(request, project_slug: str, version_id: int):
+    return HttpResponse(f"Version {version_id}, hopefully of project {project_slug}.")
